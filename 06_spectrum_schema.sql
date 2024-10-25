7d0f872f960f1af28d1e0918744d42bf199b1a31
Fernando Medina Corey <fmcorey@gmail.com>
2024-10-25 02:07:10 -0700

    Add spectrum queries

---
+++ 06_spectrum_schema.sql
@@ -0,0 +1,5 @@
+create external schema myspectrum_schema 
+from data catalog 
+database 'myspectrum_db' 
+iam_role default
+create external database if not exists;
---
+++ 07_new_external_table.sql
@@ -0,0 +1,16 @@
+create external table myspectrum_schema.sales(
+salesid integer,
+listid integer,
+sellerid integer,
+buyerid integer,
+eventid integer,
+dateid smallint,
+qtysold smallint,
+pricepaid decimal(8,2),
+commission decimal(8,2),
+saletime timestamp)
+row format delimited
+fields terminated by '\t'
+stored as textfile
+location 's3://redshift-downloads/tickit/spectrum/sales/'
+table properties ('numRows'='172000');
